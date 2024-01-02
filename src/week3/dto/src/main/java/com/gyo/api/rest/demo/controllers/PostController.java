b62b8f9a224484ad3b110ff20242873f4fc1c088
LIG-JY <dldlsry053@naver.com>
2024-01-03 02:21:08 +0900

    Feat : CrossOrigin 어노테이션으로 CORS 구현

--- src/week3/dto/src/main/java/com/gyo/api/rest/demo/controllers/PostController.java
+++ src/week3/dto/src/main/java/com/gyo/api/rest/demo/controllers/PostController.java
@@ -1,9 +1,9 @@
 package com.gyo.api.rest.demo.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gyo.api.rest.demo.dtos.PostDto;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,43 +12,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/posts")
 public class PostController {
 
-    private final ObjectMapper objectMapper;
-
-    public PostController(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    private final List<PostDto> postDtoList = new ArrayList<>(List.of(
+            new PostDto("1", "제목1", "내용1"),
+            new PostDto("2", "제목2", "내용2"),
+            new PostDto("3", "제목3", "내용3")
+    ));
 
     // Read
     @GetMapping("")
-    public List<PostDto> list(HttpServletResponse response) {
-
-        response.addHeader("Access-Control-Allow-Origin", "*");
-
-        return List.of(
-                new PostDto("1", "제목1", "내용1"),
-                new PostDto("2", "제목2", "내용2"),
-                new PostDto("3", "제목3", "내용3")
-        );
-    }
-
-    @GetMapping("/{id}")
-    public PostDto detail(
-            @PathVariable("id") String id
-    ) {
-        // Return DTO
-        return new PostDto(id, "제목", "Test Content");
+    public List<PostDto> list() {
+        return postDtoList;
     }
 
     // Create
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public String create(@RequestBody PostDto body) {
+
+        this.postDtoList.add(new PostDto("1004", body.getTitle(), body.getContent()));
+
         return body.getId() + body.getContent() + body.getTitle();
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public PostDto delete(@PathVariable String id) {
+        PostDto found = postDtoList.stream()
+                .filter(postDto -> postDto.getId().equals(id))
+                .findFirst()
+                .get();
+
+        this.postDtoList.remove(found);
+
+        return found;
     }
 }
